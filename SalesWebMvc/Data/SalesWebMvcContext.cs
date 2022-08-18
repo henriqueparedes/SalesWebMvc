54cbcf0edc00dfaf51821dcb466c4d2c002d5243
Henrique Paredes <henriqueparedes@hotmail.com>
2022-08-17 23:16:26 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -15,5 +15,7 @@ namespace SalesWebMvc.Data
         }
 
         public DbSet<SalesWebMvc.Models.Departament> Departament { get; set; } = default!;
+        public DbSet<SalesWebMvc.Models.Seller> Seller { get; set; } = default!;
+        public DbSet<SalesWebMvc.Models.SalesRecord> SalesRecord { get; set; } = default!;
     }
 }
